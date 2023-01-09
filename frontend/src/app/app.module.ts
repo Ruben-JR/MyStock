f6e14dfb62bdda4f896722a0d4bc7df922a763d5
Ruben-JR <75695011+Ruben-JR@users.noreply.github.com>
2023-01-09 08:37:58 -0100

    Frontend (#4)
    * create frontend angular project
    
    * create sidnavbar

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BodyComponent } from './components/body/body.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { ProductComponent } from './components/product/product.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BodyComponent,
+    SidenavComponent,
+    ProductComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/src/app/components/sidenav/nav-data.ts
@@ -0,0 +1,22 @@
+export const navbarData = [
+    {
+        routerLink: 'home',
+        icon: 'fal fa-home',
+        label: 'Home'
+    },
+    {
+        routerLink: 'product',
+        icon: 'fal fa-box-open',
+        label: 'Products'
+    },
+    {
+        routerLink: 'statistics',
+        icon: 'fal fa-chart-bar',
+        label: 'Statistics'
+    },
+    {
+        routerLink: 'settings',
+        icon: 'fal fa-cog',
+        label: 'Settings'
+    }
+];
