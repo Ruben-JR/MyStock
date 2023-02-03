bff9d211871b2d26daa76ac097a9119e7de88e8d
Ruben-JR <rubenpina758@gmail.com>
2023-02-03 15:26:58 -0100

    create database connection

--- backend/api/connect.php
+++ backend/api/connect.php
@@ -1,3 +1,12 @@
 <?php
     
+    $db_url = "";
+    $db_user = "";
+    $db_password = "";
+    $db_name = "";
+
+    $con = mysqli_connect($db_url, $db_user, $db_password, $db_name);
+    if (!$con) {
+        echo "Connction failed" .mysqli_connect_error();
+    }
 ?>
