eaa2c36e6c8350508a897005e17f0b30e509260b
Ruben-JR <rubenpina758@gmail.com>
2023-02-06 15:55:58 -0100

    try to connect with .env

--- backend/api/connect.php
+++ backend/api/connect.php
@@ -1,6 +1,8 @@
 <?php
-    
-    $db_url = "";
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+
+    $db_url = $_ENV['_db_url'];
     $db_user = "";
     $db_password = "";
     $db_name = "";
@@ -9,4 +11,3 @@
     if (!$con) {
         echo "Connction failed" .mysqli_connect_error();
     }
-?>
