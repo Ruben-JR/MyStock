675a060eb71ff3d845e6d69e6de801dd728e1ae1
Ruben-JR <rubenpina758@gmail.com>
2023-02-14 14:44:29 -0100

    CRUD done

--- backend/api/create.php
+++ backend/api/create.php
@@ -1,4 +1,4 @@
-<?php declare(strict_types=1);
+<?php 
     require "connect.php";
 
     //get the posted data
@@ -6,7 +6,7 @@
     if(isset($postdata) && !empty($postdata)){
         // Extract the data
         $request = json_decode($postdata);
-        if(trim($request->data->fornecedor) === '' || ($request->data->designacao) === '' || ($request->data->fabricante) === '' || (int)$request->data->numRef < 1 || ($request->data->lote) < 1 || ($request->data->testeEmbal) < 1 || ($request->data->apres) === '' || ($request->data->precoEuro) < 1 || ($request->data->precoEscudo) < 1) {
+        if(trim($request->data->fornecedor) === '' || trim($request->data->designacao) === '' || trim($request->data->fabricante) === '' || (int)$request->data->numRef < 1 || (int)$request->data->lote < 1 || (int)$request->data->testeEmbal < 1 || trim($request->data->apres) === '' || (int)$request->data->precoEuro < 1 || (int)$request->data->precoEscudo < 1) {
             // client error response
             return http_response_code(400);
         }
@@ -15,11 +15,11 @@
         $fornecedor = mysqli_real_escape_string($con, trim($request->data->model));
         $designacao = mysqli_real_escape_string($con, trim($request->data->designacao));
         $fabricante = mysqli_real_escape_string($con, trim($request->data->fabricante));
-        $numRef = mysqli_real_escape_string($con, trim($request->data->numRef));
+        $numRef = mysqli_real_escape_string($con, (int)$request->data->numRef);
         $testeEmbal = mysqli_real_escape_string($con, trim($request->data->testeEmbal));
         $apres = mysqli_real_escape_string($con, trim($request->data->apres));
-        $precoEuro = mysqli_real_escape_string($con, trim($request->data->precoEuro));
-        $precoEscudo = mysqli_real_escape_string($con, trim($request->data->precoEscudo));
+        $precoEuro = mysqli_real_escape_string($con, (int)$request->data->precoEuro);
+        $precoEscudo = mysqli_real_escape_string($con, (int)$request->data->precoEscudo);
 
         $sql = "INSERT INTO `products`(`fornecedor`, `designacao`, `fabricante`, `numRef`, `lote`, `testEmbal`, `apres`, `precoEuro`, `precoescudo`) VALUES ( '{$fornecedor}', '{$designacao}', '{$fabricante}', '{$numRef}', '{$lote}', '{$testeEmbal}', '{$apres}', '{$precoEuro}', '{$precoEscudo}')";
         if(mysqli_query($con, $sql)) {
