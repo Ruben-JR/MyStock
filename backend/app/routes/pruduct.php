1a749937a4f2c3e06a9998f53bfa88686b915535
Ruben-JR <75695011+Ruben-JR@users.noreply.github.com>
2023-12-04 21:38:40 -0100

    create entry points and routes (#28)

--- backend/app/routes/pruduct.php
+++ backend/app/routes/pruduct.php
@@ -2,15 +2,30 @@
     namespace Product\Routes;
 
     use Symfony\Component\Routing\Route;
-
+    
     class Router {
-        public function defineRoutes() {
-            // Define your routes here, as shown in the previous response
-            // Route::post('/create-products', 'ProductController@store');
-            // Route::get('/get-products', 'ProductController@index');
-            // Route::get('/get-products-id/{id}', 'ProductController@show');
-            // Route::put('/update-products/{id}', 'ProductController@update');
-            // Route::delete('/delete-products/{id}', 'ProductController@destroy');
+        public function defineRoutes($routeCollection) {
+            // Define your routes here
+            $routeCollection->add(
+                'create_products',
+                new Route('/create-products', ['_controller' => 'ProductController@store'])
+            );
+            $routeCollection->add(
+                'get_products',
+                new Route('/get-products', ['_controller' => 'ProductController@index'])
+            );
+            $routeCollection->add(
+                'get_products_id',
+                new Route('/get-products-id/{id}', ['_controller' => 'ProductController@show'])
+            );
+            $routeCollection->add(
+                'update_products',
+                new Route('/update-products/{id}', ['_controller' => 'ProductController@update'])
+            );
+            $routeCollection->add(
+                'delete_products',
+                new Route('/delete-products/{id}', ['_controller' => 'ProductController@destroy'])
+            );
         }
     }
 ?>
